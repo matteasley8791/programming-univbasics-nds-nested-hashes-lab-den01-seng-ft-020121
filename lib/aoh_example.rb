43eff5449641cca61533139754244003e9e904a9
Matthew Easley <matteasley8791@gmail.com>
2021-01-20 20:58:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,8 @@ end
 def literal_aoh [
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  {:name =>"Don Gately"
+  {:name =>"Don Gately, :occupation => "Live-in Staff"},
+  {:name =>"
   ]
 end
 
