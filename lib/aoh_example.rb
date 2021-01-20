400018d1e76de210092ac63dea1ebdcfbf7ca1a4
Matthew Easley <matteasley8791@gmail.com>
2021-01-20 21:01:55 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -12,7 +12,7 @@ def assembled_aoh
   [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
 end
 
-def literal_aoh [
+def literal_aoh = [
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   {:name=>"Don Gately", :occupation=>"Live-in Staff"},
